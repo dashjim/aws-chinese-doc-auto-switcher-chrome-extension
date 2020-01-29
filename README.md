e1aa910b042783059cc47c3298643d189915e76b
Jim <dashjim@gmail.com>
2020-01-29 17:27:08 +0800

    v1

---
+++ README.md
@@ -0,0 +1,12 @@
+Simple Chrome extension that automatically redirect your AWS pages to the Chinese version if possible.
+
+#How to in stall?
+* Enther [Chrome://extension](chrome://extension)
+* Enable the developer mode.
+* Click button `Load unpacket` 
+* Load the folder.
+
+
+Pull request is welcoming.
+
+Apache 2.0 License
---
+++ background.js
@@ -0,0 +1,20 @@
+chrome.tabs.onUpdated.addListener(
+  function(tabId, changeInfo, tab) {
+    // read changeInfo data and do something with it
+    // like send the new url to contentscripts.js
+    var currentUrl = tab.url;
+    var tabUrl = tab.url;
+    if (currentUrl.includes("docs.aws.amazon.com") && !currentUrl.includes("/zh_cn/")) {
+           var myNewUrl = tabUrl.replace("docs.aws.amazon.com", "docs.aws.amazon.com/zh_cn");
+           chrome.tabs.update(tab.id, {url: myNewUrl});
+           return;
+    }
+    if (currentUrl.includes("aws.amazon.com") && !currentUrl.includes("/cn/")) {
+           var myNewUrl = tabUrl.replace("aws.amazon.com", "amazonaws-china.com/cn");
+           chrome.tabs.update(tab.id, {url: myNewUrl});
+           return;
+    }
+  }
+);
+
+
